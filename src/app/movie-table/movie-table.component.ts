ea38c78675d20f1533acf0995df1b31cb6e7ff1a
Sagar Saji <sagarsaji7@gmail.com>
2023-06-05 14:21:58 +0530

    parentchild

---
+++ src/app/movie-table/movie-table.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-table',
+  templateUrl: './movie-table.component.html',
+  styleUrls: ['./movie-table.component.css']
+})
+export class MovieTableComponent {
+
+  @Input()
+  movieList: Array<string> = [];
+
+  @Output()
+  movieSelectedEventEmitter =  new EventEmitter();
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+  movieSelected(selectedMovie: string) { 
+    this.movieSelectedEventEmitter.emit(selectedMovie)
+  }
+
+}
